78de5e7a7021592f9b7ec150a1eb77b0213d3494
Gate Tangchartsiri <80967917+gateTang@users.noreply.github.com>
2022-07-29 17:38:50 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -375,8 +375,8 @@ def stockInput():
             #---D 
         if resetState == '1':
             del stockListed[:]
-            resetState = 0
-    return render_template("home.html", stockListed = stockListed, lookBackPeriod=lookBackPeriod, topStocks=topStocks)
+            resetState = '0'
+    return render_template("home.html", stockListed=stockListed, lookBackPeriod=lookBackPeriod, topStocks=topStocks)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
